2028f1c78dab1a08ae3406229ebcb1ba3e4ca448
Denis Mikhaylovskiy <denis.mikhalovskiy@kiwi.com>
2023-09-06 12:03:00 +0200

    Initial Commit

---
+++ YetAnotherWetherApp/YetAnotherWetherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YetAnotherWetherAppApp.swift
+//  YetAnotherWetherApp
+//
+//  Created by denis mikhaylovsky on 06.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct YetAnotherWetherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
